700fe7d93444e8b750bcd7c7225aefd3029f7ac5
Simi oluwatomi <deifilius1@gmail.com>
2024-05-21 16:06:39 +0100

    feat: :bug: throw an exception if any required database environment variable is not set

--- config/database.js
+++ config/database.js
@@ -8,6 +8,10 @@ let protocol = process.env.DB_CONNECTION;
 
 let databaseName = process.env.DB_DATABASE;
 
+if (! host || ! port || ! protocol || ! databaseName) {
+    throw new Error('One or more required environment variables (DB_HOST, DB_PORT, DB_CONNECTION, DB_DATABASE) are missing or empty.');
+}
+
 let databaseString = `${protocol}://${host}:${port}`
 
 const client = new MongoClient(databaseString);
